69894765749fde8b1a23914b87e62037d30f0652
Pushkin-hub <pushkinanton2002@gmail.com>
2025-09-10 20:32:37 +0300

    ggggggggggggggg

--- lesson2/index.py
+++ lesson2/index.py
@@ -8,6 +8,7 @@
 #     print('В доступе отказано!')
 
 
+
 # while True:
 #     number = input('Введите число: ')
 #     if number.isdigit():
@@ -28,15 +29,47 @@
     
 
 
+# names = 'Alex Bob Sam'
+# names += 'Nick'
+
+# print(names)
+# # names[0] = 'a'
+# list_names = ['Alex', 'Bob', 'Sam', 'Sam', 'Sam']
+# print(list_names)
+# list_names[0] = 'Anna'
+# print(list_names)
+
+# list_names += ['Nick']
+# list_names.append('Max')
+
+# list_names.pop(0)
+# required_name = input('Введите имя которое хотите удалить: ')
+# for name in list_names:
+#     if name == required_name:
+#         list_names.remove(name)
+
+# print(list_names)
 
 
 
+# list_nums = [i for i in range(1, 10,2)]
+# # for i in range(1, 10, 2):
+# #         list_nums.append(1)
+
+# print(list_nums)
 
 
 
+list_names = ['Alex', 'Bob', 'Samuel', 'Sam',]
+list_names.sort()
+print(list_names)
 
+replased_name = input('Какое имя вы хотите заменить?\n')
+name = input('На какое имя вы хотите заменить?\n')
+index = list_names.index(replased_name)
+list_names.insert(index, name)
 
-
+print(list_names)
 
 
 
