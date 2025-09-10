d286c80d719302e7f6e5db94ef5b0f6fea106cd1
Pushkin-hub <pushkinanton2002@gmail.com>
2025-09-10 19:32:56 +0300

    jjjjjjjjjjj

---
+++ lesson2/index.py
@@ -0,0 +1,66 @@
+# age = 60
+
+# if  age > 30:
+#     print('комната 1')
+# elif age >= 18:
+#     print('комната 2')
+# else:
+#     print('В доступе отказано!')
+
+
+# while True:
+#     number = input('Введите число: ')
+#     if number.isdigit():
+#         number = int(number)
+#         print('Строка преобразована в число')
+#         break
+#     else:
+#         print('Неверный формат')
+        
+# if number % 2 == 0 and number % 5 == 0:
+#     print('FizzBuzz')
+# elif number % 2 == 0:
+#     print('Fizz')
+# elif number % 5 == 0:
+#     print('Buzz')
+# else:
+#     print('Bad')
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
